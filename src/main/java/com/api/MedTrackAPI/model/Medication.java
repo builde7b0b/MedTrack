51b740bfb217b57316c6cab1e9fecca1370e69b5
builde7b0b <ggives3@gmail.com>
2023-06-01 09:13:17 -0700

    implement class Medication

--- src/main/java/com/api/MedTrackAPI/model/Medication.java
+++ src/main/java/com/api/MedTrackAPI/model/Medication.java
@@ -10,4 +10,39 @@ public class Medication {
     private int quantity;
 
     private LocalDate expirationDate;
+
+    public Medication() {
+    }
+
+    public Long getMedicationId() {
+        return medicationId;
+    }
+
+    public void setMedicationId(Long medicationId) {
+        this.medicationId = medicationId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 }
