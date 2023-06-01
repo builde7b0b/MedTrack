4c26577f84dbe98a82d6e0fef5edb29472d25ee5
builde7b0b <ggives3@gmail.com>
2023-06-01 08:37:58 -0700

    setup config files

---
+++ src/main/java/com/api/MedTrackAPI/MedTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.MedTrackAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedTrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedTrackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/MedTrackAPI/MedTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.MedTrackAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedTrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
