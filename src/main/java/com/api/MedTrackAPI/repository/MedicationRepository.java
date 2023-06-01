e6e567495db36a79f8e37015ef3fae9b9e087c1e
builde7b0b <ggives3@gmail.com>
2023-06-01 09:46:21 -0700

    implement interfaces for Medication and Supplier

--- src/main/java/com/api/MedTrackAPI/repository/MedicationRepository.java
+++ src/main/java/com/api/MedTrackAPI/repository/MedicationRepository.java
@@ -1,6 +1,13 @@
 package com.api.MedTrackAPI.repository;
 
+import com.api.MedTrackAPI.model.Medication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MedicationRepository extends JpaRepository {
+
+//  specify the entity type and the primary key type: <Medication, Long>
+//Medication is specified as the entity type, and Long is specified as the primary key type
+
+//Why do we extend the JPA?
+//to inherit the CRUD methods to talk with database and perform operations
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
 }
