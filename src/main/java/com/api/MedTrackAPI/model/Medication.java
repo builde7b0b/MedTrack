a2ff3ef8ad37a1786c49e5f3d3736586c24ea632
builde7b0b <ggives3@gmail.com>
2023-06-01 09:12:07 -0700

    define variables or columns of the table

--- src/main/java/com/api/MedTrackAPI/model/Medication.java
+++ src/main/java/com/api/MedTrackAPI/model/Medication.java
@@ -1,4 +1,13 @@
 package com.api.MedTrackAPI.model;
 
+import java.time.LocalDate;
+
 public class Medication {
+
+    private Long medicationId;
+    private String name;
+
+    private int quantity;
+
+    private LocalDate expirationDate;
 }
