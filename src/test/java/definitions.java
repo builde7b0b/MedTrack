9c535b7c69b1c1701ec05b198ca092a4a38b71f7
builde7b0b <ggives3@gmail.com>
2023-06-01 12:46:54 -0700

    start implementing step definitions to pass test

---
+++ src/test/java/definitions.java
@@ -0,0 +1,12 @@
+import com.api.MedTrackAPI.model.Medication;
+import io.cucumber.java.en.Given;
+
+public class definitions {
+    private Medication medication
+    @Given("I have a valid medication payload")
+    public void iHaveAValidMedicationPayload() {
+        //create a valid medication object with specific values
+        medication = new Medication();
+        medi
+    }
+}
