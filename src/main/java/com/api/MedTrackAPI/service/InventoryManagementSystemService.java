3bf5ddcdc25b88b6dfee81ef18dd16545dc14649
builde7b0b <ggives3@gmail.com>
2023-06-02 12:20:40 -0700

    annotate class

--- src/main/java/com/api/MedTrackAPI/service/InventoryManagementSystemService.java
+++ src/main/java/com/api/MedTrackAPI/service/InventoryManagementSystemService.java
@@ -2,10 +2,17 @@ package com.api.MedTrackAPI.service;
 
 import com.api.MedTrackAPI.InventoryManagementSystem;
 import com.api.MedTrackAPI.model.Medication;
+import com.api.MedTrackAPI.repository.MedicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class InventoryManagementSystemService implements InventoryManagementSystem {
+
+    @Autowired
+    private MedicationRepository medicationRepository;
     @Override
     public List<Medication> getCurrentInventory() {
         return null;
