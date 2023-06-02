d43e23b381377e8d8f9bdf91252505e11deb1a61
builde7b0b <ggives3@gmail.com>
2023-06-02 10:26:34 -0700

    start step definitions for inventory feature

---
+++ src/test/java/definitions/definitions.java
@@ -0,0 +1,15 @@
+package definitions;
+
+import com.api.MedTrackAPI.model.Medication;
+import io.cucumber.java.en.Given;
+
+import java.time.LocalDate;
+
+public class definitions {
+    private Medication medication;
+
+
+    @Given("I am a pharmacy manager")
+    public void iAmAPharmacyManager() {
+    }
+}
