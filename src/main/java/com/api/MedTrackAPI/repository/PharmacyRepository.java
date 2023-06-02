4a664df4040926288f8759318da93659a6f5bd09
builde7b0b <ggives3@gmail.com>
2023-06-02 10:41:37 -0700

    first test passing, verify user is pharm manager

--- src/main/java/com/api/MedTrackAPI/repository/PharmacyRepository.java
+++ src/main/java/com/api/MedTrackAPI/repository/PharmacyRepository.java
@@ -4,6 +4,6 @@ import com.api.MedTrackAPI.model.Pharmacy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repositorye
+@Repository
 public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
 }
