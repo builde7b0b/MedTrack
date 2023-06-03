32995885950a1fa8e8a636aa120c80eb5f09391c
builde7b0b <ggives3@gmail.com>
2023-06-02 17:10:34 -0700

    implement supplier controller class

---
+++ src/main/java/com/api/MedTrackAPI/controller/SupplierController.java
@@ -0,0 +1,28 @@
+package com.api.MedTrackAPI.controller;
+
+import com.api.MedTrackAPI.model.Supplier;
+import org.springframework.web.bind.annotation.*;
+
+public class SupplierController {
+
+    private SupplierService supplierService;
+
+    public Supplier createSupplier(@RequestBody Supplier supplier) {
+        return supplierService.createSupplier(supplier);
+    }
+
+    @GetMapping("/{supplierId})")
+    public Supplier getSupplier(@PathVariable Long supplierId) {
+        return supplierService.getSupplier(supplierId);
+    }
+
+    @PutMapping("/{supplierId}")
+    public Supplier updateSuplier(@PathVariable Long supplierId, @RequestBody Supplier supplier) {
+        return supplierService.updateSupplier(supplierId, supplier);
+    }
+
+    @DeleteMapping("/{supplierId}")
+    public void deleteSupplier(@PathVariable Long supplierId) {
+        supplierService.deleteSupplier(suppierId);
+    }
+}
