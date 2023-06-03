47a774656362ec51400f3f69981e876f4338bf49
builde7b0b <ggives3@gmail.com>
2023-06-02 17:15:11 -0700

    start implementation of Service for Supplier

--- src/main/java/com/api/MedTrackAPI/service/SupplierService.java
+++ src/main/java/com/api/MedTrackAPI/service/SupplierService.java
@@ -1,4 +1,14 @@
 package com.api.MedTrackAPI.service;
 
+import com.api.MedTrackAPI.repository.SupplierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SupplierService {
+
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    public Sup
 }
