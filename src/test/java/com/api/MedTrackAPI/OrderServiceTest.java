5efab2d46bf57e8333830042c92d59410ac087b0
builde7b0b <ggives3@gmail.com>
2023-06-06 10:18:02 -0700

    add comments

--- src/test/java/com/api/MedTrackAPI/OrderServiceTest.java
+++ src/test/java/com/api/MedTrackAPI/OrderServiceTest.java
@@ -15,6 +15,9 @@ import java.util.List;
 import static org.mockito.Mockito.when;
 
 //UNIT TESTING
+//using JUnit 5 for testing
+// using Mockito for mocking dependencies
+
 public class OrderServiceTest {
     @Mock
     private OrderRepository orderRepository;
@@ -22,17 +25,24 @@ public class OrderServiceTest {
     @InjectMocks
     private OrderService orderService;
 
+    /**
+     * initializes teh mocks using the only line inside
+     */
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
     }
 
+    /**
+     * performs the unit test
+     * create two mock orders
+     */
     @Test
     public void testGenerateReorderSuggestions(){
         //implement unit test to verify the generation of the reorder suggestions
         //based on stock thresholds
 
-        //create mock data
+        //create mock data to be returned by the mocked order Repo with find all
         Order order1 = new Order();
         Order order2 = new Order();
         List<Order> mockOrders = List.of(order1, order2);
@@ -41,10 +51,10 @@ public class OrderServiceTest {
         when(orderRepository.findAll()).thenReturn(mockOrders);
 
 
-        // call the method under test
+        // call the method under test and store result into variable
         List<Order> reorderSuggestions = orderService.generateReorderSuggestions();
 
-        // assert the result
+        // assert the result and verify the list matches expected results
         Assertions.assertEquals(mockOrders, reorderSuggestions);
 
 
