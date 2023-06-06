f8f23a77717a1248b15d6c7f3580ca64fde585d6
builde7b0b <ggives3@gmail.com>
2023-06-06 10:13:50 -0700

    create mock data

--- src/test/java/com/api/MedTrackAPI/OrderServiceTest.java
+++ src/test/java/com/api/MedTrackAPI/OrderServiceTest.java
@@ -1,11 +1,15 @@
 package com.api.MedTrackAPI;
 
+import com.api.MedTrackAPI.model.Order;
 import com.api.MedTrackAPI.repository.OrderRepository;
-import com.api.MedTrackAPI.service.OrderService
+import com.api.MedTrackAPI.service.OrderService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.List;
 
 //UNIT TESTING
 public class OrderServiceTest {
@@ -20,9 +24,23 @@ public class OrderServiceTest {
         MockitoAnnotations.openMocks(this);
     }
 
+    @Test
     public void testGenerateReorderSuggestions(){
         //implement unit test to verify the generation of the reorder suggestions
         //based on stock thresholds
+
+        //create mock data
+        Order order1 = new Order();
+        Order order2 = new Order();
+        List<Order> mockOrders = List.of(order1, order2);
+
+        //mock behavior of orderRepository
+
+
+        // call the method under test
+
+        // assert the result
+
 
     }
 
