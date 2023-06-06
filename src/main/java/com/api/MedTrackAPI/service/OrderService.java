31b4915fe29157a94d22cb424ce2742f2d8ea886
builde7b0b <ggives3@gmail.com>
2023-06-06 09:43:36 -0700

    implement Order service

--- src/main/java/com/api/MedTrackAPI/service/OrderService.java
+++ src/main/java/com/api/MedTrackAPI/service/OrderService.java
@@ -1,4 +1,25 @@
 package com.api.MedTrackAPI.service;
 
+import com.api.MedTrackAPI.model.Order;
+import com.api.MedTrackAPI.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public List<Order>  generateReorderSuggestions(){
+        //implement the logic to calc stock levels, compare with thresholds,
+        // and generate reorder suggestions
+        //return list of orders as reorder suggestions
+        return null;
+    }
+
+
 }
